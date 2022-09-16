02065a4957c3817c7ea1c60f66d5535c19fb551b
Serdar Okcira <serdarokcira@SerdarOcirasMBP.iconos.be>
2022-09-16 13:04:19 +0200

    adding mapstruct, mappers, decorators, caching, dtos etc.

--- src/main/java/guru/sfg/beer/inventory/service/bootstrap/BeerInventoryBootstrap.java
+++ src/main/java/guru/sfg/beer/inventory/service/bootstrap/BeerInventoryBootstrap.java
@@ -2,7 +2,6 @@ package guru.sfg.beer.inventory.service.bootstrap;
 
 import guru.sfg.beer.inventory.service.domain.BeerInventory;
 import guru.sfg.beer.inventory.service.repositories.BeerInventoryRepository;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -13,47 +12,43 @@ import java.util.UUID;
  * Created by jt on 2019-06-07.
  */
 @Slf4j
-@RequiredArgsConstructor
 @Component
-public class BeerInvntoryBootstrap implements CommandLineRunner {
-    public static final String BEER_1_UPC = "0631234200036";
-    public static final String BEER_2_UPC = "0631234300019";
-    public static final String BEER_3_UPC = "0083783375213";
+public class BeerInventoryBootstrap implements CommandLineRunner {
     public static final UUID BEER_1_UUID = UUID.fromString("0a818933-087d-47f2-ad83-2f986ed087eb");
+    public static final String BEER_1_UPC = "0631234200036";
     public static final UUID BEER_2_UUID = UUID.fromString("a712d914-61ea-4623-8bd0-32c0f6545bfd");
-    public static final UUID BEER_3_UUID = UUID.fromString("026cc3c8-3a0c-4083-a05b-e908048c1b08");
+    public static final String BEER_2_UPC = "0083783375213";
 
     private final BeerInventoryRepository beerInventoryRepository;
+
+    public BeerInventoryBootstrap(BeerInventoryRepository beerInventoryRepository) {
+        this.beerInventoryRepository = beerInventoryRepository;
+    }
 
     @Override
     public void run(String... args) throws Exception {
         if(beerInventoryRepository.count() == 0){
+            System.out.println("is beeer inventory repo 0 " + beerInventoryRepository.count());
             loadInitialInv();
         }
     }
 
     private void loadInitialInv() {
-        beerInventoryRepository.save(BeerInventory
+        BeerInventory bI1 = BeerInventory
                 .builder()
                 .beerId(BEER_1_UUID)
                 .upc(BEER_1_UPC)
-                .quantityOnHand(50)
-                .build());
+                .quantityOnHand(22)
+                .build();
 
-        beerInventoryRepository.save(BeerInventory
+        BeerInventory bI2 = BeerInventory
                 .builder()
                 .beerId(BEER_2_UUID)
                 .upc(BEER_2_UPC)
-                .quantityOnHand(50)
-                .build());
+                .quantityOnHand(39)
+                .build();
 
-        beerInventoryRepository.saveAndFlush(BeerInventory
-                .builder()
-                .beerId(BEER_3_UUID)
-                .upc(BEER_3_UPC)
-                .quantityOnHand(50)
-                .build());
-
-        log.debug("Loaded Inventory. Record count: " + beerInventoryRepository.count());
+        beerInventoryRepository.save(bI1);
+        beerInventoryRepository.save(bI2);
     }
 }
